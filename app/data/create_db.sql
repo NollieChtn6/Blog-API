945218fc11b1ebba53ce9b4c98f27264bc4b9c44
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-05-27 14:41:54 +0200

    create scripts to create and seed tables in database

---
+++ app/data/create_db.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+DROP TABLE IF EXISTS 
+"categories",
+"posts";
+
+-- -------------------
+-- Table "categories"
+-- -------------------
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    slug VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL
+);
+
+-- -------------------
+-- Table "posts"
+-- -------------------
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    categoryId INT NOT NULL,
+    slug VARCHAR(255) NOT NULL UNIQUE,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(50) NOT NULL,
+    excerpt TEXT,
+    content TEXT,
+    FOREIGN KEY (categoryId) REFERENCES categories(id)
+);
+
+COMMIT;
